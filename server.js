d67eb98f690ab5aa6570f18cf722ed5ee9b3e797
Technophile <patricia.snodgrass@gmail.com>
2018-06-18 01:42:52 -0500

    adjusted hbs

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
-// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+// If deployed, use the deployed database. Otherwise use the local gossipmongo database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/gossipmongo";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
